bf48ae5093c232c6f1a9573b7df289588df24c18
samuel jones <skj9000@gmail.com>
2024-12-02 15:56:43 +0800

    action goal pram chnaged

--- loop_pres/scripts/loop.py
+++ loop_pres/scripts/loop.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     # check the pal_presentation_msgs/PlayPresentationGoal message
     # definition for the possible goal parameters
-    goal = pal_presentation_msgs.msg.PlayPresentationFromNameActionGoal(presentation_name="joondalup")
+    goal = pal_presentation_msgs.msg.PlayPresentationFromNameActionGoal(goal.presentation_name="joondalup")
 
     client.send_goal(goal)
     client.wait_for_result()
