9b95c677807d733a21d74ba653d86476c3fffb56
samuel jones <skj9000@gmail.com>
2024-12-02 14:32:37 +0800

    trting to play presentation in loop script

---
+++ loop_pres/scripts/loop.py
@@ -0,0 +1,18 @@
+import rospy
+import actionlib
+from pal_presentation_msgs.msg import *
+
+
+if __name__ == "__main__":
+
+    client = actionlib.SimpleActionClient("/pal_play_presentation", pal_presentation_msgs.msg.PlayPresentationAction)
+    client.wait_for_server()
+
+    # check the pal_presentation_msgs/PlayPresentationGoal message
+    # definition for the possible goal parameters
+    goal = pal_presentation_msgs.msg.PlayPresentationGoal("joondalup")
+
+    client.send_goal(goal)
+    client.wait_for_result()
+
+    rospy.loginfo("Action returned: %s" % client.get_result())
