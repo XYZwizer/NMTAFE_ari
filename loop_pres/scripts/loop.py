e95e1d87c6a1d4e0c15d2509342ffa7bf9b4e296
samuel jones <skj9000@gmail.com>
2024-12-02 15:42:05 +0800

    loop now useds PlayPresentationFromName

--- loop_pres/scripts/loop.py
+++ loop_pres/scripts/loop.py
@@ -7,12 +7,12 @@ from pal_presentation_msgs.msg import *
 if __name__ == "__main__":
     rospy.init_node('NMTAFE_loop_presentation')
 
-    client = actionlib.SimpleActionClient("/pal_play_presentation", pal_presentation_msgs.msg.PlayPresentationAction)
+    client = actionlib.SimpleActionClient("/pal_play_presentation_from_name", pal_presentation_msgs.msg.PlayPresentationAction)
     client.wait_for_server()
 
     # check the pal_presentation_msgs/PlayPresentationGoal message
     # definition for the possible goal parameters
-    goal = pal_presentation_msgs.msg.PlayPresentationGoal("joondalup")
+    goal = pal_presentation_msgs.msg.PlayPresentationFromNameActionGoal(presentation_name="joondalup")
 
     client.send_goal(goal)
     client.wait_for_result()
