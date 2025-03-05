0af660fb62900ea4e1f70bed524b6148fee03594
MasterAirscrachDev <airscrach@gmail.com>
2025-03-05 16:42:14 +0800

    puppeteer

---
+++ Ari_Puppeteer/scripts/main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import Gamepad
+import time
+from geometry_msgs.msg import Twist
+import rospy
+
+class Puppeteer:
+	def __init__(self):
+		self.gamePad = GamepadInterface()
+		self.movementInterface = RosMovementInterface()
+
+	def Tick(self):
+		(x, y) = self.gamePad.GetInput("LEFT-X", "axis"), self.gamePad.GetInput("LEFT-Y", "axis")
+		self.movementInterface.SetMovement((x, y), 0.3)
+
+class GamepadInterface:
+	def __init__(self):
+		self.gamePadType = Gamepad.Xbox360
+		self.gamePad = None
+
+	def GetInput(self, inputName: str, inputType: str):
+		if(self.gamePad is not None and self.gamePad.isConnected()):
+			if(inputType == "pressed"):
+				return self.gamePad.isPressed(inputName)
+			elif(inputType == "axis"):
+				return self.gamePad.axis(inputName)
+		else:
+			if(self.gamePad is None):
+				if(Gamepad.available()):
+					self.gamePad = self.gamePadType()
+					self.gamePad.startBackgroundUpdates()
+			elif(not self.gamePad.isConnected()):
+				self.gamePad.disconnect()
+				self.gamePad = None
+			else:
+				print("Gamepad is not connected")
+			return None
+		
+
+
+class RosMovementInterface:
+	def __init__(self):
+		self.hasPublisher = False
+		rospy.init_node('ari_controller_teli_NMTAFE2')
+		self.publisher = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size = 1)
+		self._wait_for_subscribers(self.publisher)
+
+	def _wait_for_subscribers(self, publisher):
+		i = 0
+		while not rospy.is_shutdown() and publisher.get_num_connections() == 0:
+			if i == 4:
+				print("Waiting for subscriber to connect to {}".format(publisher.name))
+			rospy.sleep(0.5)
+			i += 1
+			i = i % 5
+			if rospy.is_shutdown():
+				raise Exception("Got shutdown request before subscribers connected")
+		self.hasPublisher = True
+
+	def _toTwist(self, direc : tuple, speed : float) -> Twist:
+		twist_msg = Twist()
+		twist_msg.linear.x = -direc[1] * speed
+		twist_msg.linear.y = 0
+		twist_msg.linear.z = 0
+		twist_msg.angular.x = 0
+		twist_msg.angular.y = 0
+		twist_msg.angular.z = -direc[0] * speed
+		return twist_msg
+	
+	def SetMovement(self, movement, speed):
+		if self.hasPublisher:
+			self.publisher.publish(self._toTwist(movement, speed))
+		else:
+			print("No publisher available")
+	
+	
+
+if __name__ == "__main__":
+	ari = Puppeteer()
+	while not rospy.is_shutdown():
+		ari.Tick()
