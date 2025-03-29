239346a6a1b614851b86beac6eee1653ebeb77b8
samuel jones <skj9000@gmail.com>
2025-03-29 12:42:29 +0800

    move file structure around to work better and be more clear also remove catkin projects that where duplicates or with names that didn’t make sense. These file can still be found in the last commit

--- NMTAFE_ari_controller/README.md
+++
@@ -1,2 +0,0 @@
-# NMTAFE_ari_controller
-a ros package for using a controllor gamepad to control the ari robot by pal robotics
--- NMTAFE_ari_controller/scripts/main_headless.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from node_manager import ari_mover
-
-if __name__ == "__main__":
-	ari = ari_mover()
-	while(1):
-		ari.do()
-		
