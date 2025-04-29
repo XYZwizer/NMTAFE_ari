fb605a6585633efa847d5b962170aacee9ceef6b
Samuel jones <skj9000@gmail.com>
2025-04-29 03:41:58 +0000

    screen test

--- nmtafe_ws/src/NMTAFE_gandalf_game/scripts/main.py
+++ nmtafe_ws/src/NMTAFE_gandalf_game/scripts/main.py
@@ -7,6 +7,8 @@ from actionlib import SimpleActionClient
 from emoji_to_emote import do_emot_from_emoji
 from am_looked_at import BodyOrientationListener
 
+import pal_web_msgs
+
 import rospy
 
 # class syntax
@@ -23,13 +25,20 @@ class NMTAFE_gandalf_game_node:
 			'/humans/voices/anonymous_speaker/speech',
 			LiveSpeech,
 			self.on_speech)
-	
-	rospy.Subscriber("/NMTAFE_gandalf_game/password_attempt", String, callback)
-	
+		
+		self.pas_sub = rospy.Subscriber("/NMTAFE_gandalf_game/password_attempt", String, callback)
+		
+		self.web_pub = rospy.Publisher('/web/go_to',  pal_web_msgs.WebGoTo, queue_size=10) 
+		
 		self.game_state = game_state.awating_player
 	
+	def set_page(self,page_name):
+		new_page = pal_web_msgs.WebGoTo(type=pal_web_msgs.WebGoTo.TOUCH_PAGE,value=page_name)
+		self.web_pub.publish(new_page)
+
 	def set_game_state(self,new_state):
 		print(self.game_state,new_state)
+                self.set_page("gandalf_game_password_input")
 
 	def on_speech(self,text):
 		if "yes" in text:
